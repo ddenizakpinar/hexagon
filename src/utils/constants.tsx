ffe9efc9503f549ee8926bf6c93cfd06bf5982cc
Deniz Akpınar <ddenizakpinar@gmail.com>
2022-02-12 20:19:43 +0300

    Add gifs

--- src/utils/constants.tsx
+++ src/utils/constants.tsx
@@ -1,5 +1,5 @@
-export const nftContractAddress = "0xd9AEF4bD48D3040D54202F97AB2DaE12236DFDc0";
-export const nftMarketContractAddress = "0x327d9d4A097c0e4372b4ea7E620f82f78B29E269";
+export const nftContractAddress = "0x57e3A313F0f3E6e7F5D1d55CB5be86933Aa8562E";
+export const nftMarketContractAddress = "0x8B406BDC50285571824209bE2322d29Fe029d157";
 
 export enum Pages {
   EXPLORE = "/",
